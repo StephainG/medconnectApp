2c337f3c6b9c3cb0893c65382ebacfde07cabb4a
StephainG <onibukunoluwa@yahoo.com>
2023-06-04 17:45:16 +0100

    update

--- medconnect/medconnectApp/urls.py
+++ medconnect/medconnectApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import AppointmentTemplateView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
     path('signout', views.signout, name='signout'),
     path('signin', views.signin, name='signin'),
+    path('make-an-appointment', AppointmentTemplateView.as_view(), name="appointment"),
 ]
--- medconnect/medconnectApp/views.py
+++ medconnect/medconnectApp/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.views.generic.base import TemplateView
+from .models import Appointment
 from medconnect import settings
 from django.core.mail import send_mail
 
@@ -67,3 +69,25 @@ def signout(request):
     logout(request)
     # messages.info(request, 'Logged out successfully')
     return redirect('/')
+
+
+class AppointmentTemplateView(TemplateView):
+    template_name = "appointment.html"
+
+    def post(self, request):
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        email = request.POST.get("email")
+        mobile = request.POST.get("mobile")
+        message = request.POST.get("request")
+
+        appointment = Appointment.objects.create(
+            first_name=fname,
+            last_name=lname,
+            email=email,
+            phone=mobile,
+            request=message,
+        )
+
+        messages.add_message(request, messages.SUCCESS, f"{fname.upper()}, Thank you. We will contact you as soon as possible")
+        return HttpResponseRedirect(request.path)
